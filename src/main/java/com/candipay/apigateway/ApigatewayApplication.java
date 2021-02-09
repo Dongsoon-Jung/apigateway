93b8c0544365207e1916ac2f68fc5bffd2e4b568
dosjung <dosjung@hanwha.com>
2021-02-09 23:02:41 +0900

    Initial Commit

---
+++ src/main/java/com/candipay/apigateway/ApigatewayApplication.java
@@ -0,0 +1,23 @@
+package com.candipay.apigateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Slf4j
+@RestController
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public void test() {
+		log.info("test-api called...");
+	}
+}
